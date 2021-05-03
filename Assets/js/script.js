9b8badb180605ad4c681a257f79e9bd46ecc60a1
Aniruddha Bhattacharya <p.aniruddhabhattacharya@gmail.com>
2021-05-02 19:47:04 -0700

    Completed | Works, added basic bootstrap, does all fxns | Looks very ugly

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,5 @@
 var searchHistory = localStorage.getItem('searchHistory')? JSON.parse(localStorage.getItem('searchHistory')):[];
-//var searchQuery = '';
-//var apiKey = d07c8b701886b8b4c094668aee47dc1a;
+//var apiKey = d07c8b701886b8b4c094668aee47dc1a; // Tried to seperate API key --- FAILED
 var gcwQuery = function(query) {
     console.log(query);
     fetch(
@@ -23,12 +22,9 @@ var gcwQuery = function(query) {
             return wDetailsResponse.json();
         })
         .then(function(wDetailsResponse){
-            /*var responseArr = [wResponse, wDetailsResponse];*/
             var responseObj = {name:locationName, details: wDetailsResponse}
             //console.log(responseObj);
             buildCWCard(responseObj);
-            //buildCWCard(responseArr);
-            //buildCWCard(wResponse, wDetailsResponse);
             return responseObj;
         });
         
@@ -61,61 +57,75 @@ var buildCWCard = function(response) {
     var currentDate = new Date(response.details.current.dt*1000);
     currentDate = currentDate.toLocaleDateString();
     var titleEl = document.createElement("h2");
-    titleEl.innerHTML = response.name+ " "+currentDate+" "+ "<img src = ./Assets/img/"+response.details.current.weather[0].icon+"@2x.png >";
+    titleEl.innerHTML = response.name+ " "+currentDate+ " <img src = ./Assets/img/"+response.details.current.weather[0].icon+"@2x.png >";
     currWeatherDiv.appendChild(titleEl);
     var tempEl = document.createElement("p");
-    tempEl.innerHTML = "Temperature: "+ response.details.current.temp;
+    tempEl.innerHTML = "Temperature: "+ response.details.current.temp + "°F";
     currWeatherDiv.appendChild(tempEl);
     var humEl = document.createElement("p");
-    humEl.innerHTML = "Humidity: "+ response.details.current.humidity;
+    humEl.innerHTML = "Humidity: "+ response.details.current.humidity + "%";
     currWeatherDiv.appendChild(humEl);
     var windSpEl = document.createElement("p");
-    windSpEl.innerHTML = "Wind Speed: "+ response.details.current.wind_speed;
+    windSpEl.innerHTML = "Wind Speed: "+ response.details.current.wind_speed +" MPH";
     currWeatherDiv.appendChild(windSpEl);
     var uvEl = document.createElement("p");
     uvEl.innerHTML = "UV Index: "+ response.details.current.uvi;
+    /*switch (response.details.current.uvi) { // Switch case attempt ----- FAILED
+        case (response.details.current.uvi < 3):
+            uvEl.className = 'bg-success';
+            break;
+        case (response.details.current.uvi < 6):
+            uvEl.className = ' bg-warning';
+            break;
+        case (response.details.current.uvi < 11):
+            uvEl.className = ' bg-danger';
+            break;
+    }*/
+    if (response.details.current.uvi < 3) {
+        uvEl.className = 'bg-success text-white';
+    }
+    else if (response.details.current.uvi < 6) {
+        uvEl.className = ' bg-warning text-dark';
+    }
+    else if (response.details.current.uvi < 6) {
+        uvEl.className = ' bg-danger text-white';
+    }
+    else {
+        uvEl.className = ' bg-secondary text-white';
+    }
     currWeatherDiv.appendChild(uvEl);
 
     buildForcastCards(response.details.daily);
-/*
-city name, - response.name
-the date, - response.details
-an icon representation of weather conditions, -response.details
-the temperature, -response.details
-the humidity, -response.details
-the wind speed, -response.details
-and the UV index -response.details
-*/
 }
 
-var buildForcastCards = function(fResponse) {
+var buildForcastCards = function(fResponse) { // Builds forcast cards with 5 forcasts each
     console.log(fResponse);
     var weatherForcastDiv = document.getElementById("current-forcast");
     weatherForcastDiv.innerHTML = '';
-    for(var i = 0; i <((fResponse.length)-2); i++){
-        var forcastDiv = document.createElement("div");
+    for(var i = 1; i <((fResponse.length)-2); i++){ // in order to get next 5 days I had to start at index i and go all the way to 2 before the max length of the array(fResponse.length - 2) 
+        var forcastDiv = document.createElement("div"); // Create a div element called forcast
+        forcastDiv.className = "card m-2"; // assign a class card to make a card using bootstrap | used m-2 to create margin
         //Date
-        var fDate = document.createElement("h4");
-        var forcastDate = new Date(fResponse[i].dt*1000);
-        forcastDate = forcastDate.toLocaleDateString();
-        fDate.innerHTML = forcastDate;
-        forcastDiv.appendChild(fDate);
+        var fDate = document.createElement("h4"); // create a h4 element
+        var forcastDate = new Date(fResponse[i].dt*1000); // store date value in forcast date. Had to *1000 to get correct val from unix
+        forcastDate = forcastDate.toLocaleDateString(); // used .toLocaleDateString to just get the date
+        fDate.innerHTML = forcastDate; // assign fDates innerHTML to be forcast Date
+        forcastDiv.appendChild(fDate); // append fDate to forcastDiv
         //weatherIcon
         var fIcon = document.createElement("img");
         fIcon.setAttribute('src','./Assets/img/'+fResponse[i].weather[0].icon + '@2x.png');
         forcastDiv.appendChild(fIcon);
         //temp
         var fTemp = document.createElement("p");
-        fTemp.innerHTML = "Temp: "+ fResponse[i].temp.day;
+        fTemp.innerHTML = "Temp: "+ fResponse[i].temp.day + "°F";
         forcastDiv.appendChild(fTemp);
         //Humidity
         var fHumi = document.createElement("p");
-        fHumi.innerHTML = "Humidity: "+ fResponse[i].humidity;
+        fHumi.innerHTML = "Humidity: "+ fResponse[i].humidity + "%";;
         forcastDiv.appendChild(fHumi);
 
         weatherForcastDiv.appendChild(forcastDiv);
     }
-    /*Build Current weather card*/
 }
 
 var addSearchHistory = function(searchParam) {
@@ -124,13 +134,16 @@ var addSearchHistory = function(searchParam) {
         if (searchHistory[i] === searchParam) {
             //console.log("delete "+searchHistory[i] + "\nbecause " + searchHistory[i]+" === "+ searchParam);
             searchHistory.splice(i, 1);
+            populateSearchHistory();
         }
     }
     if (searchHistory.length > 5) {
         //console.log("List is too long")
         searchHistory.shift();
+        populateSearchHistory();
     }
     searchHistory.push(searchParam);
+    populateSearchHistory();
     //console.log(searchHistory);
     localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
 }
@@ -139,9 +152,9 @@ var populateSearchHistory = function() {
     var searchHistoryList = document.getElementById("search-history-list");
     console.log(searchHistory);
     searchHistoryList.innerHTML = '';
-    for(var i = searchHistory.length - 1; i >=0; i--){ // Had to do this to iterate and order the saved search list correctly
+    for(var i = searchHistory.length - 1; i >=0; i--){ // Had to do this to iterate and order the saved search list a bit more cleanly
         console.log(searchHistory[i]);
-        //var cityName = searchHistory[i].toString();
+        //var cityName = searchHistory[i].toString(); // Tried to use jquery for this----- failed miserably
         /*var searchEl = $("<li>")
             .text(cityName);
             searchHistoryList.append(searchEl);*/
